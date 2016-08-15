3d8cea8a5465d8861b2c7d645456c91ce9959f7d
Ondrej Mosnáček <omosnacek@gmail.com>
2016-08-15 21:34:43 +0200

    Document the argon2_select_impl() function.

--- include/argon2-opt.h
+++ include/argon2-opt.h
@@ -3,6 +3,11 @@
 
 #include <stdio.h>
 
+/**
+ * Selects the fastest available optimized implementation.
+ * @param out The file for debug output (e. g. stderr; pass NULL for no
+ * debug output)
+ */
 void argon2_select_impl(FILE *out);
 
 #endif // ARGON2_OPT_H
