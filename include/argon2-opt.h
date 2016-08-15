fcc297ee4983a2f6c03e0c9c9645e488e0b2a3d6
Ondrej Mosnáček <omosnacek@gmail.com>
2016-08-15 21:38:10 +0200

    Use conventions from argon2.h in argon2-opt.h.

--- include/argon2-opt.h
+++ include/argon2-opt.h
@@ -3,12 +3,29 @@
 
 #include <stdio.h>
 
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+/* Symbols visibility control */
+#ifdef A2_VISCTL
+#define ARGON2_PUBLIC __attribute__((visibility("default")))
+#elif _MSC_VER
+#define ARGON2_PUBLIC __declspec(dllexport)
+#else
+#define ARGON2_PUBLIC
+#endif
+
 /**
  * Selects the fastest available optimized implementation.
  * @param out The file for debug output (e. g. stderr; pass NULL for no
  * debug output)
  */
 void argon2_select_impl(FILE *out);
+
+#if defined(__cplusplus)
+}
+#endif
 
 #endif // ARGON2_OPT_H
 
