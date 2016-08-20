268a5f6936d21fff51483774fb7a3fbba926b3a7
Ondrej Mosnáček <omosnacek@gmail.com>
2016-08-20 19:08:29 +0200

    Use dynamic number of samples in bench2

--- src/bench2.c
+++ src/bench2.c
@@ -14,14 +14,18 @@
 #include "timing.h"
 
 #define ARGON2_BLOCK_SIZE 1024
-#define MAX_MEM_COST (1024 * 1024)
+
+#define BENCH_MAX_T_COST 8
+#define BENCH_MAX_M_COST (1024 * 1024)
+#define BENCH_MAX_THREADS 4
+#define BENCH_MIN_PASSES (1024 * 1024)
+#define BENCH_MAX_SAMPLES 128
 
 #define BENCH_OUTLEN 16
 #define BENCH_INLEN 16
-#define BENCH_SAMPLES 16
 
-uint8_t *static_memory = NULL;
-size_t static_memory_size = 0;
+static uint8_t *static_memory = NULL;
+static size_t static_memory_size = 0;
 
 static int allocate_static(uint8_t **memory, size_t bytes_to_allocate)
 {
@@ -57,11 +61,11 @@ static int benchmark(uint32_t t_cost, uint32_t m_cost, uint32_t p)
 
     unsigned char out[BENCH_OUTLEN];
     struct timestamp start, end;
-    double ms_d[BENCH_SAMPLES];
-    double ms_i[BENCH_SAMPLES];
+    double ms_d[BENCH_MAX_SAMPLES];
+    double ms_i[BENCH_MAX_SAMPLES];
 
     double ms_d_final, ms_i_final;
-    unsigned int i;
+    unsigned int i, bench_samples;
     argon2_context ctx;
 
     int res;
@@ -84,7 +88,13 @@ static int benchmark(uint32_t t_cost, uint32_t m_cost, uint32_t p)
     ctx.free_cbk = deallocate_static;
     ctx.flags = ARGON2_DEFAULT_FLAGS;
 
-    for (i = 0; i < BENCH_SAMPLES; i++) {
+    bench_samples = (BENCH_MIN_PASSES * p) / (t_cost * m_cost);
+    bench_samples += (BENCH_MIN_PASSES * p) % (t_cost * m_cost) != 0;
+
+    if (bench_samples > BENCH_MAX_SAMPLES) {
+        bench_samples = BENCH_MAX_SAMPLES;
+    }
+    for (i = 0; i < bench_samples; i++) {
         timestamp_store(&start);
         res = argon2d_ctx(&ctx);
         timestamp_store(&end);
@@ -95,7 +105,7 @@ static int benchmark(uint32_t t_cost, uint32_t m_cost, uint32_t p)
         ms_d[i] = timestamp_span_ms(&start, &end);
     }
 
-    for (i = 0; i < BENCH_SAMPLES; i++) {
+    for (i = 0; i < bench_samples; i++) {
         timestamp_store(&start);
         res = argon2i_ctx(&ctx);
         timestamp_store(&end);
@@ -106,8 +116,8 @@ static int benchmark(uint32_t t_cost, uint32_t m_cost, uint32_t p)
         ms_i[i] = timestamp_span_ms(&start, &end);
     }
 
-    ms_d_final = min(ms_d, BENCH_SAMPLES);
-    ms_i_final = min(ms_i, BENCH_SAMPLES);
+    ms_d_final = min(ms_d, bench_samples);
+    ms_i_final = min(ms_i, bench_samples);
 
     printf("%8lu%16lu%8lu%16.3lf%16.3lf\n",
            (unsigned long)t_cost, (unsigned long)m_cost, (unsigned long)p,
@@ -122,7 +132,7 @@ int main(void)
 
     argon2_select_impl(stderr, "[libargon2] ");
 
-    static_memory_size = (size_t)MAX_MEM_COST * (size_t)ARGON2_BLOCK_SIZE;
+    static_memory_size = (size_t)BENCH_MAX_M_COST * (size_t)ARGON2_BLOCK_SIZE;
     static_memory = malloc(static_memory_size);
     if (static_memory == NULL) {
         return 1;
@@ -132,9 +142,9 @@ int main(void)
 
     printf("%8s%16s%8s%16s%16s\n", "t_cost", "m_cost", "threads",
            "Argon2d (ms)", "Argon2i (ms)");
-    for (t_cost = 1; t_cost <= 8; t_cost *= 2) {
-        for (m_cost = 1024; m_cost <= MAX_MEM_COST; m_cost *= 4) {
-            for (p = 1; p <= 4; p *= 2) {
+    for (t_cost = 1; t_cost <= BENCH_MAX_T_COST; t_cost *= 2) {
+        for (m_cost = 1024; m_cost <= BENCH_MAX_M_COST; m_cost *= 4) {
+            for (p = 1; p <= BENCH_MAX_THREADS; p *= 2) {
                 res = benchmark(t_cost, m_cost, p);
                 if (res != 0) {
                     free(static_memory);
