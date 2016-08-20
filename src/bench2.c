0e85c63b0be4e923a88eb4bae4a08a9b0768681a
Ondrej Mosnáček <omosnacek@gmail.com>
2016-08-20 18:37:19 +0200

    Use preallocated memory buffer in bench2
    Traversing a freshly-allocated buffer incurs a lot of page faults, which messes up the benchmarks. By pre-allocating the memory and offering it to libargon2 via a custom allocator, we can eliminate this unpredictable overhead.

--- src/bench2.c
+++ src/bench2.c
@@ -13,9 +13,30 @@
 
 #include "timing.h"
 
+#define ARGON2_BLOCK_SIZE 1024
+#define MAX_MEM_COST (1024 * 1024)
+
 #define BENCH_OUTLEN 16
 #define BENCH_INLEN 16
 #define BENCH_SAMPLES 16
+
+uint8_t *static_memory = NULL;
+size_t static_memory_size = 0;
+
+static int allocate_static(uint8_t **memory, size_t bytes_to_allocate)
+{
+    if (static_memory_size < bytes_to_allocate) {
+        return ARGON2_MEMORY_ALLOCATION_ERROR;
+    }
+    *memory = static_memory;
+    return ARGON2_OK;
+}
+
+static void deallocate_static(uint8_t *memory, size_t bytes_to_allocate)
+{
+    (void)memory;
+    (void)bytes_to_allocate;
+}
 
 static double min(const double *samples, size_t count)
 {
@@ -41,13 +62,31 @@ static int benchmark(uint32_t t_cost, uint32_t m_cost, uint32_t p)
 
     double ms_d_final, ms_i_final;
     unsigned int i;
+    argon2_context ctx;
 
     int res;
 
+    ctx.out = out;
+    ctx.outlen = sizeof(out);
+    ctx.pwd = (uint8_t *)PASSWORD;
+    ctx.pwdlen = sizeof(PASSWORD);
+    ctx.salt = (uint8_t *)SALT;
+    ctx.saltlen = sizeof(SALT);
+    ctx.secret = NULL;
+    ctx.secretlen = 0;
+    ctx.ad = NULL;
+    ctx.adlen = 0;
+    ctx.t_cost = t_cost;
+    ctx.m_cost = m_cost;
+    ctx.lanes = ctx.threads = p;
+    ctx.version = ARGON2_VERSION_NUMBER;
+    ctx.allocate_cbk = allocate_static;
+    ctx.free_cbk = deallocate_static;
+    ctx.flags = ARGON2_DEFAULT_FLAGS;
+
     for (i = 0; i < BENCH_SAMPLES; i++) {
         timestamp_store(&start);
-        res = argon2d_hash_raw(t_cost, m_cost, p, PASSWORD, sizeof(PASSWORD),
-                               SALT, sizeof(SALT), out, sizeof(out));
+        res = argon2d_ctx(&ctx);
         timestamp_store(&end);
         if (res != ARGON2_OK) {
             return res;
@@ -58,8 +97,7 @@ static int benchmark(uint32_t t_cost, uint32_t m_cost, uint32_t p)
 
     for (i = 0; i < BENCH_SAMPLES; i++) {
         timestamp_store(&start);
-        res = argon2i_hash_raw(t_cost, m_cost, p, PASSWORD, sizeof(PASSWORD),
-                               SALT, sizeof(SALT), out, sizeof(out));
+        res = argon2i_ctx(&ctx);
         timestamp_store(&end);
         if (res != ARGON2_OK) {
             return res;
@@ -84,18 +122,27 @@ int main(void)
 
     argon2_select_impl(stderr, "[libargon2] ");
 
+    static_memory_size = (size_t)MAX_MEM_COST * (size_t)ARGON2_BLOCK_SIZE;
+    static_memory = malloc(static_memory_size);
+    if (static_memory == NULL) {
+        return 1;
+    }
+    /* make sure the whole memory gets mapped to physical pages: */
+    memset(static_memory, 0xAB, static_memory_size);
+
     printf("%8s%16s%8s%16s%16s\n", "t_cost", "m_cost", "threads",
            "Argon2d (ms)", "Argon2i (ms)");
-
     for (t_cost = 1; t_cost <= 8; t_cost *= 2) {
-        for (m_cost = 1024; m_cost <= 1024 * 1024; m_cost *= 4) {
+        for (m_cost = 1024; m_cost <= MAX_MEM_COST; m_cost *= 4) {
             for (p = 1; p <= 4; p *= 2) {
                 res = benchmark(t_cost, m_cost, p);
                 if (res != 0) {
+                    free(static_memory);
                     return res;
                 }
             }
         }
     }
+    free(static_memory);
     return 0;
 }
